d3b4ed973573557d09d82861e71f2d3bf19b9c09
rgm3 <rgm3@users.noreply.github.com>
2022-05-09 15:17:23 -0500

    Add switch statement from Caustic on Discord

--- DTMF_to_pulse.ino
+++ DTMF_to_pulse.ino
@@ -22,10 +22,22 @@ int buffer_position = 0;
 
 volatile unsigned long last_dialed_time = 0;
 
+#define stq_pin 3 //for nano, can only use D2 or D3 for interrupt pins.
+#define q1_pin 4
+#define q2_pin 5
+#define q3_pin 6
+#define q4_pin 7
 
 void setup() {
   // Attach interrupt to the MT8870 StD pin
-  attachInterrupt(digitalPinToInterrupt(MT8870_STD), dtmf_interrupt);
+  attachInterrupt(digitalPinToInterrupt(stq_pin), read_dtmf_inputs, FALLING);
+
+
+  pinMode(stq_pin, INPUT); // connect to Std pin
+  pinMode(q4_pin, INPUT); // connect to Q4 pin
+  pinMode(q3_pin, INPUT); // connect to Q3 pin
+  pinMode(q2_pin, INPUT); // connect to Q2 pin
+  pinMode(q1_pin, INPUT); // connect to Q1 pin
 
   // On bootup set exchange pin high
   hang_up();
@@ -108,4 +120,55 @@ void hang_up(){
 void dtmf_interrupt() {
   // Set flag, avoid function calls in interrupt
   last_dialed_time = 1;
+}
+
+void read_dtmf_inputs()
+{
+  Serial.println("Hello, I'm in read_dtmf_inputs()!");
+  Serial.println();
+
+  uint8_t number_pressed;
+  delay(250);
+  
+  // checks q1,q2,q3,q4 to see what number is pressed.
+  number_pressed = ( 0x00 | (digitalRead(7)<<0) | (digitalRead(6)<<1) | (digitalRead(5)<<2) | (digitalRead(4)<<3) );
+  switch (number_pressed)
+  {
+    case 0x01:
+    Serial.println("Button Pressed =  1");
+    break;
+    case 0x02:
+    Serial.println("Button Pressed =  2");
+    break;
+    case 0x03:
+    Serial.println("Button Pressed =  3");
+    break;
+    case 0x04:
+    Serial.println("Button Pressed =  4");
+    break;
+    case 0x05:
+    Serial.println("Button Pressed =  5");
+    break;
+    case 0x06:
+    Serial.println("Button Pressed =  6");
+    break;
+    case 0x07:
+    Serial.println("Button Pressed =  7");
+    break;
+    case 0x08:
+    Serial.println("Button Pressed =  8");
+    break;
+    case 0x09:
+    Serial.println("Button Pressed =  9");
+    break;
+    case 0x0A:
+    Serial.println("Button Pressed =  0");
+    break;
+    case 0x0B:
+    Serial.println("Button Pressed =  *");
+    break;
+    case 0x0C:
+    Serial.println("Button Pressed =  #");
+    break;    
+  }
 }
